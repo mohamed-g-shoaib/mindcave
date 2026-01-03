e2b7e0931e8de7199106a6f62e57697de2268b45
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2026-01-03 23:55:15 +0200

    refactor: change from Radix UI to Base UI and update asChild prop to render prop

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -18,11 +18,19 @@ export async function AuthButton() {
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
+      <Button
+        render={<Link href="/auth/login" />}
+        size="sm"
+        variant={"outline"}
+      >
+        Sign in
       </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
+      <Button
+        render={<Link href="/auth/sign-up" />}
+        size="sm"
+        variant={"default"}
+      >
+        Sign up
       </Button>
     </div>
   );
